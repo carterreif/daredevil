647a28d297566bac63f60e091e967f29c9ce54a5
Aaron Reifschneider <aaronreifschneider@outlook.com>
2025-01-25 13:13:52 -0600

    Updated API endpoint URL

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let uploadedImages = JSON.parse(localStorage.getItem('daredevilGallery')) || [];
 let galleryImages = [];
 
 // Get API base URL based on environment
-const apiBaseUrl = 'https://daredevil-zacjn4ufs-ajr1073s-projects.vercel.app';
+const apiBaseUrl = 'https://daredevil-hfnhewr5b-ajr1073s-projects.vercel.app';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Content Loaded');
